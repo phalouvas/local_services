7a408d74623fcb5892535d494577cc2ac81a6920
Panayiotis Halouvas <phalouvas@kainotomo.com>
2021-11-03 19:32:10 +0200

    doing laravel nginx

---
+++ images/laravel/nginx/build.sh
@@ -0,0 +1,2 @@
+docker build . --tag phalouvas/laravel:php8.0-nginx
+docker push phalouvas/laravel:php8.0-nginx
---
+++ images/laravel/nginx/entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+# Start ssh
+echo "Starting ssh..."
+mkdir -p /run/sshd
+/usr/sbin/sshd
+
+# Copy php ini overrides
+cp /home/docker/php-ini-overrides.ini /usr/local/etc/php/conf.d/99-overrides.ini
+
+# setup production
+if [ $PHCOMM_ENV = "production" ]; then
+    echo "Setup production environment..."
+    cd /home/code
+    echo "Copying code..."
+    cp -r -f * /var/www/html
+    cp .env.azure /var/www/html/.env
+    if [ -f .htaccess ]; then
+        cp .htaccess /var/www/html/.htaccess
+    fi
+    cd /var/www/html
+    echo "Installing dependencies..."
+    composer install
+    if [ $PHCOMM_BUILD_ASSETS = "true" ]; then
+        echo "Building assets..."
+        npm install
+        npm run production
+    fi
+    echo "Database migrating..."
+    php artisan migrate --force
+    echo "Clearing cache..."
+    php artisan clear-compiled
+    php artisan config:clear
+    php artisan config:cache
+    php artisan view:clear
+    php artisan view:cache
+    php artisan route:clear
+    php artisan route:cache
+fi
+
+# setup local
+if [ $PHCOMM_ENV = "local" ]; then
+    echo "Setup local environment..."
+    cd /var/www/html
+    echo "Copying env file..."
+    cp .env.local .env
+
+    if [ $PHCOMM_SUPERVISOR = "true" ]; then
+        echo "Starting supervisor..."
+        cd /home/docker
+        cp crontab /etc/cron.d/phcomm
+        cp supervisord.local.conf /etc/supervisor/conf.d/phcomm.conf
+        /usr/bin/supervisord
+    fi
+
+fi
+
+# Fix permissions
+echo "Fixing permissions..."
+cd /var/www/html
+chown -R www-data:www-data public
+chown -R www-data:www-data storage
+
+# Start php-fpm
+php-fpm -D
+nginx -g 'daemon off;'
+
+exec "$@"
